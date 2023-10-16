b64980abd103550368e29c9f91407bee195de4f7
Michaela Laitinen <kaylait2324@gmail.com>
2023-10-16 14:42:14 -0500

    WIP

--- src/Wavelength/models.py
+++ src/Wavelength/models.py
@@ -2,4 +2,6 @@ from django.db import models
 
 # Create your models here.
 class FileUpload(models.Model):
-    file = models.FileField()
+    file = models.FileField(upload_to='uploads/')
+    
+##changed file = models.FileField() to file = models.FileField(upload_to='uploads/')

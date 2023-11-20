d3d5f3d2da52124b776514b1cbf1507bda48270c
Christina Hoover <ninamae02@gmail.com>
2023-11-20 12:01:19 -0600

    State saving, preprossesing works from front end

--- src/Wavelength/urls.py
+++ src/Wavelength/urls.py
@@ -6,11 +6,14 @@ from . import views
 
 #URL Config
 urlpatterns = [
-    path("", views.home, name= "homepage"),
+    path("home", views.home, name= "homepage"),
     path("presentation", views.presentation, name= "presentation"),
     path("settings/graphs", views.settings1, name= "settings-graphs"),
     path("settings/electrode-topography", views.settings2, name="settings-electrode-topography"),
-    path("settings/image-map", views.settings3, name="settings-image-map")
+    path("settings/image-map", views.settings3, name="settings-image-map"),
+    path("settings/graphs/regression", views.regression_handling, name="regression"),
+    path("settings/graphs/filtering", views.filter_handling, name="filtering"),
+    path("settings/graphs/ica", views.ica_handling, name="ica"),
 ]
 
 if settings.DEBUG:
--- src/Wavelength/views.py
+++ src/Wavelength/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from .models import FileUpload
 import mne
-import pandas as pd  
+from mne.preprocessing import EOGRegression, ICA
 import numpy as np                
 import plotly.graph_objects as go 
 import plotly.io as pio
@@ -9,18 +9,57 @@ from plotly.offline import plot
 from plotly.graph_objs import Layout, YAxis, Scatter, Annotation, Annotations, Data, Figure, Marker, Font
 mne.set_log_level('WARNING')
 
- # Process the uploaded file here (e.g., using MNE)
-def handle_uploaded_file(f): 
-   # Read and save raw data
-   raw = mne.io.read_raw_fif(f, preload=True)
-   print(raw.info) 
-   #Preprossing with filtering
-   raw.filter(1,20)
-   epoch_plot=epoch_handler(raw)
-   channel_plot= channel_picks(raw)
 
-   return epoch_plot, channel_plot
+def filter_handling(request):
+     raw.crop(0, 50).pick_types(meg="mag", stim=True).load_data()
+     raw_downsampled = raw.copy().resample(sfreq=200)
+     print(raw.info)
+     return HttpResponse("Filter Handled.")
 
+def regression_handling(request):
+    raw.pick(["eeg", "eog", "stim"])
+    raw.load_data()
+    raw.set_eeg_reference("average")
+    raw.filter(0.3, 40)
+
+    events = mne.find_events(raw)
+    event_id = {"visual/left": 3, "visual/right": 4}
+    epochs = mne.Epochs(raw, events, event_id=event_id, preload=True)
+    model_plain = EOGRegression(picks="eeg", picks_artifact="eog").fit(epochs)
+    epochs_clean_plain = model_plain.apply(epochs)
+    epochs_clean_plain.apply_baseline()
+    print(raw.info)
+    return HttpResponse("Regression Handled.")
+    
+
+def ica_handling(request):
+     raw.crop(tmax=60.0).pick_types(meg="mag", eeg=True, stim=True, eog=True)
+     raw.load_data()
+     filt_raw = raw.copy().filter(l_freq=1.0, h_freq=None)
+     ica = ICA(n_components=15, max_iter="auto", random_state=97)
+     ica.fit(filt_raw)
+
+     ica.exclude = []
+     eog_indices, eog_scores = ica.find_bads_eog(raw)
+     ica.exclude = eog_indices    
+     print(raw.info)
+     return HttpResponse("ICA Handled.")
+    
+     
+
+def channel_graph(request):
+    new_raw = raw.copy()
+    chs = []
+    chan_idxs = [raw.ch_names.index(ch) for ch in chs]
+    new_raw.plot(order=chan_idxs, start=12, duration=4)
+    print(new_raw.info)
+    fig = raw.plot(order=chan_idxs, start=12, duration=4)
+    channels_html= pio.to_html(fig, full_html=True)
+    return HttpResponse("Channel graph handled.")
+      
+def events(request):
+    raw.crop(tmax=60).load_data()
+    return HttpResponse("Events graph handled.")
    
 # def events(raw):
 #    events = mne.find_events(raw, stim_channel="STI 014")
@@ -30,67 +69,67 @@ def handle_uploaded_file(f):
 #    events_html= pio.to_html(fig, full_html=True)
 #    return events_html
 
-def channel_picks(raw):
-   picks = mne.pick_types(raw.info, meg='grad', exclude=[])
-   start, stop = raw.time_as_index([0,10])
+# def channel_picks(raw):
+#    picks = mne.pick_types(raw.info, meg='grad', exclude=[])
+#    start, stop = raw.time_as_index([0,10])
    
-   n_channels = 20
-   data, times = raw[picks[:n_channels], start:stop]
-   ch_names = [raw.info['ch_names'][p] for p in picks[:n_channels]]
-   step = 1. / n_channels
-   kwargs = dict(domain=[1 - step, 1], showticklabels=False, zeroline=False, showgrid=False)
+#    n_channels = 20
+#    data, times = raw[picks[:n_channels], start:stop]
+#    ch_names = [raw.info['ch_names'][p] for p in picks[:n_channels]]
+#    step = 1. / n_channels
+#    kwargs = dict(domain=[1 - step, 1], showticklabels=False, zeroline=False, showgrid=False)
 
    
-   layout = Layout(yaxis=go.layout.YAxis(kwargs), showlegend=False)
-   traces = [Scatter(x=times, y=data.T[:, 0])]
+#    layout = Layout(yaxis=go.layout.YAxis(kwargs), showlegend=False)
+#    traces = [Scatter(x=times, y=data.T[:, 0])]
 
 
-   for ii in range(1, n_channels):
-        kwargs.update(domain=[1 - (ii + 1) * step, 1 - ii * step])
-        layout.update({'yaxis%d' % (ii + 1): go.layout.YAxis(kwargs), 'showlegend': False})
-        traces.append(Scatter(x=times, y=data.T[:, ii], yaxis='y%d' % (ii + 1)))
+#    for ii in range(1, n_channels):
+#         kwargs.update(domain=[1 - (ii + 1) * step, 1 - ii * step])
+#         layout.update({'yaxis%d' % (ii + 1): go.layout.YAxis(kwargs), 'showlegend': False})
+#         traces.append(Scatter(x=times, y=data.T[:, ii], yaxis='y%d' % (ii + 1)))
 
   
-   annotations = Annotations([go.layout.Annotation(x=-0.06, y=0, xref='paper', yref='y%d' % (ii + 1),
-                                      text=ch_name, font=go.layout.annotation.Font(size=9), showarrow=False)
-                          for ii, ch_name in enumerate(ch_names)])
-   layout.update(annotations=annotations)
-   layout.update(autosize=False, width=1000, height=600)
-   fig = Figure(data=Data(traces), layout=layout)
-   channels_html= pio.to_html(fig, full_html=True)
-   return channels_html
+#    annotations = Annotations([go.layout.Annotation(x=-0.06, y=0, xref='paper', yref='y%d' % (ii + 1),
+#                                       text=ch_name, font=go.layout.annotation.Font(size=9), showarrow=False)
+#                           for ii, ch_name in enumerate(ch_names)])
+#    layout.update(annotations=annotations)
+#    layout.update(autosize=False, width=1000, height=600)
+#    fig = Figure(data=Data(traces), layout=layout)
+#    channels_html= pio.to_html(fig, full_html=True)
+#    return channels_html
 
       
-def epoch_handler(raw):
-   #Preprosses with ICA
-   ica = mne.preprocessing.ICA(n_components=20,random_state=0)    
-   ica.fit(raw)
-   bad_ica, scores = ica.find_bads_eog(raw, 'MEG 0122', threshold =2)   
+# def epoch_handler(raw):
+#    #Preprosses with ICA
+#    ica = mne.preprocessing.ICA(n_components=20,random_state=0)    
+#    ica.fit(raw)
+#    bad_ica, scores = ica.find_bads_eog(raw, 'MEG 0122', threshold =2)   
    
-   events = mne.find_events(raw, stim_channel="STI 014")
-   event_dict = {
-    "auditory/left": 1,
-    "auditory/right": 2,
-    "visual/left": 3,
-    "visual/right": 4,
-    "face": 5,
-    "buttonpress": 32,
-   }
+#    events = mne.find_events(raw, stim_channel="STI 014")
+#    event_dict = {
+#     "auditory/left": 1,
+#     "auditory/right": 2,
+#     "visual/left": 3,
+#     "visual/right": 4,
+#     "face": 5,
+#     "buttonpress": 32,
+#    }
  
-   epochs = mne.Epochs(raw, events, tmin=-0.3, tmax=0.7, event_id=event_dict, preload=True) 
-   epochs = ica.apply(epochs,exclude=ica.exclude)
-   epochs.apply_baseline((None,0))
+#    epochs = mne.Epochs(raw, events, tmin=-0.3, tmax=0.7, event_id=event_dict, preload=True) 
+#    epochs = ica.apply(epochs,exclude=ica.exclude)
+#    epochs.apply_baseline((None,0))
    
-   values = epochs.get_data().mean(axis=0)
-   values = pd.DataFrame(np.transpose(values), columns=epochs.info['ch_names'])
-   values.head()
+#    values = epochs.get_data().mean(axis=0)
+#    values = pd.DataFrame(np.transpose(values), columns=epochs.info['ch_names'])
+#    values.head()
    
-   fig = go.Figure(layout=dict(xaxis=dict(title='time'),yaxis=dict(title='data')))
-   for ch in epochs.info['ch_names']:
-      fig.add_scatter(x=epochs.times, y=values[ch], name=ch)
+#    fig = go.Figure(layout=dict(xaxis=dict(title='time'),yaxis=dict(title='data')))
+#    for ch in epochs.info['ch_names']:
+#       fig.add_scatter(x=epochs.times, y=values[ch], name=ch)
 
-   epochs_html= pio.to_html(fig, full_html=True)
-   return epochs_html
+#    epochs_html= pio.to_html(fig, full_html=True)
+#    return epochs_html
    
 
 def settings1(request):
@@ -111,6 +150,9 @@ def home (request):
     #In here, you can pull data, transform data, etc
      if request.method == 'POST':
         newfile = request.FILES['file']
+        global raw
+        raw = mne.io.read_raw_fif(newfile, verbose=False, preload=True)
+        print(raw.info)
         document = FileUpload.objects.create(file=newfile)
         document.save()
         return redirect('settings-graphs')
